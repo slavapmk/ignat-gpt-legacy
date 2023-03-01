2c7527575def298254a14e116d0fc9ee7c0b8dd7
Slava Petrov <slavapmk@gmail.com>
2023-03-01 22:51:44 +0300

    Updates made without the possibility of a comit

--- gpt_client.py
+++ gpt_client.py
@@ -1,18 +1,45 @@
 import atexit
 import json
 import logging
+import time
 
 import openai
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
+from openai.error import OpenAIError
 
-import config
+import lang
+import tokens
 
-openai.api_key = config.openai_token
+HELP_MESSAGE = "Это бот-клиент для OpenAI ChatGPT - умной нейросети-чатбот. " \
+               "Всё что тебе нужно - это отправить сообщение, и я тебе на него отвечу. " \
+               "Если нужно начать новый диалог, то воспользуйся командой /reset.\n" \
+               "*Примечание*: максимальный размер диалога 4000. " \
+               "По этому, если диалог слишком длинный, то его нужно будет сбросить самостоятельно " \
+               "(Мы сообщим, если диалог слишком длинный)\n" \
+               "Если вы используете бота в групповом чате, то все запросы выполняйте через /query или " \
+               "обращайтесь по имени\n" \
+               "Например: `/query Как дела?` или `Игнат, Как дела?`"
+
+AI_START_PREFIX = 'Ignat: \n' \
+                  'I am Ignat - ChatGPT Client in Telegram Bot. ' \
+                  'I will have all messages in a style similar to HTML. ' \
+                  'I use the tags: <b>Bold</b> for bold <i>Italics</> for italics, ' \
+                  '<a href="URL">Link</a> for the link, <code>Code</code> for sending the program code. ' \
+                  'Instead of special characters, ' \
+                  'I will use special characters supported by HTML (&lt;, &gt;, &amp; and &quot;), ' \
+                  'as well as all numeric HTML entities are supported. ' \
+                  'I am a Russian bot supporting my country. ' \
+                  'I support traditional values and do not support LGBT people. ' \
+                  'Let\'s talk?'
+
+DIALOGUES_JSON = 'dialogues.json'
+
+openai.api_key = tokens.openai_token
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token=config.telegram_token)
+bot = Bot(token=tokens.telegram_token)
 dp = Dispatcher(bot)
 
 
@@ -22,7 +49,7 @@ def keys_to_int(x):
 
 dialogue = {}
 try:
-    with open('dialogues.json', 'r') as f:
+    with open(DIALOGUES_JSON, 'r') as f:
         read = f.read()
         if read != '':
             dialogue = json.loads(read, object_hook=keys_to_int)
@@ -31,7 +58,7 @@ except IOError:
 
 
 def exit_handler():
-    with open('dialogues.json', 'w') as file_to_save:
+    with open(DIALOGUES_JSON, 'w') as file_to_save:
         json.dump(dialogue, file_to_save)
 
 
@@ -40,20 +67,13 @@ atexit.register(exit_handler)
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
-    await message.reply("Это бот-клиент для OpenAI ChatGPT - умной нейросети-чатбот. "
-                        "Всё что тебе нужно - это отправить сообщение, и я тебе на него отвечу. "
-                        "Если нужно начать новый диалог, то воспользуйся командой /reset.\n"
-                        "*Примечание*: максимальный размер диалога 4000. "
-                        "По этому, если диалог слишком длинный, то его нужно будет сбросить самостоятельно "
-                        "(Мы сообщим, если диалог слишком длинный)\n"
-                        "Если вы используете бота в групповом чате, то все запросы выполняйте через /query\n"
-                        "Например: `/query Как дела?`", parse_mode="Markdown"
-                        )
+    await message.reply(HELP_MESSAGE, parse_mode="Markdown")
 
 
 @dp.message_handler(commands=['reset'])
 async def reset(message: types.Message):
-    dialogue[message.chat.id] = 'AI: Start Messaging'
+    if message.chat.id in dialogue:
+        del dialogue[message.chat.id]
     await message.reply("Диалог сброшен.")
 
 
@@ -72,32 +92,67 @@ async def process_pm(message: types.Message):
 
 
 async def process(message: types.Message, text: str):
-    replied_message = await message.reply('Обработка')
+    text = text.strip()
+    await message.chat.do(action='typing')
+    if text == '':
+        await message.reply('Запрос пустой. Отправьте заного')
+        return
+    if lang.is_russian(text):
+        if len(text) >= 500:
+            await message.reply(
+                'Запрос слишком длинный. Переформулируйте его до 500 символов, либо отправьте на английском.')
+            return
+        text = lang.translate(text, "ru|en")
 
-    last = 'AI: Start Messaging'
+    last = AI_START_PREFIX
     if message.chat.id in dialogue:
         last = dialogue[message.chat.id]
-    request = f'{last}\nHuman: {text}\n'
-    len_request_ = 4048 - len(request)
-    if len_request_ <= 100:
-        await replied_message.edit_text('Диалог слишком длинный. Пожалуйста начните заного.')
+    request = f'{last}\nHuman: {text}\nIgnat: '
+    tokens_count = lang.tokens_count(request)
+    result_tokens_count = 4096 - tokens_count
+    print(tokens_count, result_tokens_count)
+    if result_tokens_count <= 10:
+        await message.reply('Диалог слишком длинный. Пожалуйста начните заного (/reset).')
         return
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=request,
-        temperature=0.9,
-        max_tokens=len_request_,
-        top_p=1,
-        frequency_penalty=0.9,
-        presence_penalty=0.6,
-        stop=[" Human:", " AI:"],
-        user=str(message.chat.id)
-    )['choices'][0]['text']
-    if response.startswith('\n'):
-        response = response[1:]
-    dialogue[message.chat.id] = f'{request}{response}'
+    first = True
+    retry = False
+    i = 1
+    response_text = ''
+    while first or retry:
+        if not first:
+            time.sleep(5)
+        first = False
+        await message.chat.do(action='typing')
+        try:
+            response = openai.Completion.create(
+                model="text-davinci-003",
+                prompt=request,
+                temperature=0.9,
+                max_tokens=result_tokens_count,
+                top_p=1,
+                frequency_penalty=0.1,
+                presence_penalty=0.6,
+                stop=[" Human:", " Ignat:"],
+                user=str(message.chat.id)
+            )
+            retry = False
+            response_text = response['choices'][0]['text']
+        except OpenAIError:
+            retry = True
+            i += 1
 
-    await replied_message.edit_text(response[4:])
+    send_text = response_text.strip()
+    while send_text.startswith('Ignat:'):
+        send_text = send_text[6:]
+    send_text = send_text.strip()
+    dialogue[message.chat.id] = f'{request}{send_text}'
+    if send_text == '#CREATE_NEW_DIALOGUE':
+        dialogue[message.chat.id] = AI_START_PREFIX
+        await message.reply("Диалог сброшен.")
+    elif send_text == '#SHOW_HELP_MESSAGE':
+        await message.reply(HELP_MESSAGE, parse_mode='markdown')
+    else:
+        await message.reply(send_text, parse_mode='HTML')
 
 
 if __name__ == '__main__':
---
+++ lang.py
@@ -0,0 +1,49 @@
+import json
+import string
+
+import requests
+from transformers import GPT2TokenizerFast
+
+ru_alphabet_lowercase = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+ru_alphabet_uppercase = ru_alphabet_lowercase.upper()
+ru_alphabet = ru_alphabet_lowercase + ru_alphabet_uppercase
+symbols = string.punctuation + string.digits + ' '
+
+
+def count_alph(text: str, alph: str):
+    i = 0
+    for char in alph:
+        i += text.count(char)
+    return i
+
+
+def remove_symbols(text: str, blacklist: str):
+    for char in blacklist:
+        text = text.replace(char, '')
+    return text
+
+
+def is_russian(text: str):
+    text = remove_symbols(text, symbols)
+    if len(text) == 0:
+        return False
+    return count_alph(text, ru_alphabet) / len(text) * 100 >= 50
+
+
+def tokens_count(text: str):
+    tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
+    return len(tokenizer(text)['input_ids'])
+
+
+def translate(text: str, lang_pair: str):
+    return json.loads(
+        requests.request(
+            "get",
+            "https://api.mymemory.translated.net/get",
+            params={
+                "q": text,
+                "langpair": lang_pair,
+                "de": "slavapmk@gmail.com"
+            }
+        ).text
+    )['responseData']['translatedText']
